9d1f5059303d29430b7e56be31eae902875e3449
alex409235982 <alexc42748@posteo.com>
2026-05-27 11:32:00 -0400

    Added Docusaurus Setup

---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+import {themes} from 'prism-react-renderer';
+
+const config = {
+  title: 'Visual Plan Construct Language)',
+  tagline: 'A visual language for teaching and learning programming.',
+  favicon: 'img/favicon.ico',
+  url: 'https://vpcl.vercel.app',
+  baseUrl: '/',
+  organizationName: 'vpcl',
+  projectName: 'vpc-website',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.js',
+          editUrl: 'https://github.com/alex42837405/VPCL',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    navbar: {
+      title: 'VPCL',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/alex42837405/VPCL',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Documentation',
+          items: [
+            {
+              label: 'Introduction',
+              to: '/docs/intro',
+            },
+            {
+              label: 'VPCL Phases',
+              to: '/docs/vpcl-overview',
+            },
+            {
+              label: 'Language Library',
+              to: '/docs/language-construct-library',
+            },
+          ],
+        },
+        {
+          title: 'Research',
+          items: [
+            {
+              label: 'Original Paper',
+              href: 'https://www.sciencedirect.com/science/article/pii/1045926X9290021D',
+            },
+            {
+              label: 'Patent',
+              href: 'https://patents.google.com/patent/US20060179420A1',
+            },
+          ],
+        },
+      ],
+    },
+    prism: {
+      theme: themes.github,
+      darkTheme: themes.dracula,
+      additionalLanguages: ['pascal', 'c', 'java'],
+    },
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+  },
+};
+
+export default config;
---
+++ sidebars.js
@@ -0,0 +1,18 @@
+const sidebars = {
+  docsSidebar: [
+    'intro',
+    'programming-errors',
+    'plan-composition-errors',
+    'language-construct-misconceptions',
+    'visualization-and-programming',
+    'vpcl-overview',
+    'phase-1-plan-observation',
+    'phase-2-plan-integration',
+    'phase-3-plan-creation',
+    'language-construct-library',
+    'visualization-vs-textual',
+    'conclusions',
+  ],
+};
+
+export default sidebars;
